ef92f35570da503b6d77a9d41acc518046e08bf5
Lluis Guardiola <lluisguardiola.h@gmail.com>
2019-10-01 08:21:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -131,6 +131,9 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages] += 
+     programmer_hash[:dennis_ritchie][:languages].push = "Assembly"
+     
+     return programmer_hash
+
 
 end
